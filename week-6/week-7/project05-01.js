eb8808a81e0cad33f42eb68fc28dafdbede730e3
Leslie Espino <via.esp56@gmail.com>
2026-03-01 20:58:37 -0600

    Complete Hands-On Project 5-1 timers and intervals

---
+++ week-6/week-7/project05-01.js
@@ -0,0 +1,82 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 5
+      Project 05-01
+
+      Project to present an online quiz with a countdown clock
+      Author: Leslie Easpino
+      Date: March 1st, 2026
+
+      Filename: project05-01.js
+*/
+
+// Constants to set the time given for the quiz in seconds
+// and the correct answers to each quiz question
+const quizTime = 90;
+const correctAnswers = ["10", "4", "-6", "-5", "-7"];
+
+// Elements in the quiz page
+let startQuiz = document.getElementById("startquiz");
+let quizClock = document.getElementById("quizclock");
+let overlay = document.getElementById("overlay");
+
+// Initialize the quiz time
+quizClock.value = quizTime;
+let timeLeft = quizTime;
+
+// Declare the ID for timed commands
+// and the node list for questions
+let timeID;
+let questionList = document.querySelectorAll("div#quiz input");
+
+startQuiz.onclick = function () {
+  timeLeft = quizTime;
+  quizClock.value = timeLeft;
+
+  overlay.className = "showquiz";
+
+  clearInterval(timeID);
+  timeID = setInterval(countdown, 1000);
+};
+
+function countdown() {
+  // Decrease time and update display
+  timeLeft--;
+  quizClock.value = timeLeft;
+
+  // When time reaches 0, stop and grade immediately
+  if (timeLeft === 0) {
+    clearInterval(timeID);
+
+    let totalCorrect = checkAnswers();
+
+    if (totalCorrect === correctAnswers.length) {
+      alert("Congratulations! You got 100%!");
+    } else {
+      alert(
+        "You got " +
+          totalCorrect +
+          " correct out of " +
+          correctAnswers.length +
+          ".",
+      );
+      timeLeft = quizTime;
+      quizClock.value = timeLeft;
+      overlay.className = "hidequiz";
+    }
+  }
+}
+
+function checkAnswers() {
+  let correctCount = 0;
+
+  for (let i = 0; i < questionList.length; i++) {
+    if (questionList[i].value === correctAnswers[i]) {
+      correctCount++;
+      questionList[i].className = "";
+    } else {
+      questionList[i].className = "wronganswer";
+    }
+  }
+  return correctCount;
+}

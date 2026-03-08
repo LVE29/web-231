61e6b97e8d11c50ddbb8b71b8c4c732fda02e330
Leslie Espino <via.esp56@gmail.com>
2026-03-08 16:32:50 -0500

    Week 8 Hands-On Project 6-3

---
+++ week-8/project06-03.js
@@ -0,0 +1,57 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 6
+      Project 06-03
+
+      Script to complete a form containing billing and shipping address information
+      Author: Leslie Espino
+      Date: March 8, 2026
+
+      Filename: project06-03.js
+*/
+// reference to the "Same as Shipping Address" checkbox
+let useShip = document.getElementById("useShip");
+
+// run copyShippingToBilling when the checkbox is clicked
+useShip.addEventListener("click", copyShippingToBilling);
+
+function copyShippingToBilling() {
+  if (useShip.checked) {
+    document.getElementById("firstnameBill").value =
+      document.getElementById("firstnameShip").value;
+
+    document.getElementById("lastnameBill").value =
+      document.getElementById("lastnameShip").value;
+
+    document.getElementById("address1Bill").value =
+      document.getElementById("address1Ship").value;
+
+    document.getElementById("address2Bill").value =
+      document.getElementById("address2Ship").value;
+
+    document.getElementById("cityBill").value =
+      document.getElementById("cityShip").value;
+
+    document.getElementById("countryBill").value =
+      document.getElementById("countryShip").value;
+
+    document.getElementById("codeBill").value =
+      document.getElementById("codeShip").value;
+
+    document.getElementById("stateBill").selectedIndex =
+      document.getElementById("stateShip").selectedIndex;
+  }
+}
+let formElements = document.querySelectorAll("input[type='text']");
+let fieldCount = formElements.length;
+let errorBox = document.getElementById("errorBox");
+
+for (let i = 0; i < fieldCount; i++) {
+  formElements[i].addEventListener("invalid", showValidationError);
+}
+
+function showValidationError(evt) {
+  evt.preventDefault();
+
+  errorBox.textContent = "Complete all highlighted fields";
+}
